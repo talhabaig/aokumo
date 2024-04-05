11b421e98a850fda045438a3af46568aa06d028b
Mahad-Dev <mahadkhurshid4052@gmail.com>
2024-04-05 16:38:31 +0500

    update design

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,12 +1,13 @@
 import ChatBox from "../components/chat/ChatBox";
 import Chat from "../components/chat/Chat";
+import SuggestionMessages from "../components/chat/SuggestionMessage";
 
 export default function page() {
   return (
     <div className="flex flex-col">
       <div className="h-[calc(100vh-315px)] overflow-auto px-[20px]">
         <div className="w-full max-w-[880px] mx-auto flex flex-col gap- pt-7">
-         <Chat />
+          {false ? <Chat /> : <SuggestionMessages />}
         </div>
       </div>
       <ChatBox />
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,8 @@ module.exports = {
         1: "0px 5px 5px rgba(0,0,0,0.07)",
         2: "0px 3px 3.5px rgba(34,163,241,0.2)",
         3: "0px 5px 5px rgba(0,0,0,0.16)",
-        4: "0px 3px 3px rgba(0,0,0,0.16 )"
+        4: "0px 3px 3px rgba(0,0,0,0.16 )",
+        5: "0px 5px 5px rgba(34,163,241,0.2 )"
       },
     },
   },
